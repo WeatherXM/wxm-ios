329d23ebad1dfdea604ab2edb68e791548331bce
Pantelis Giazitsis <pgiazitsis@weatherxm.com>
2024-01-29 12:47:25 +0200

    Submit to firebase scripts

--- ci_scripts/ci_post_clone.sh
+++ ci_scripts/ci_post_clone.sh
@@ -2,9 +2,13 @@
 
 touch ~/.netrc
 
+echo "Create mapbox files"
 echo "machine api.mapbox.com" > ~/.netrc
 echo "login mapbox" >> ~/.netrc
 echo "password ${MAPBOX_TOKEN}" >> ~/.netrc
+
+echo "Install Firebase CLI"
+curl -sL https://firebase.tools | bash
 
 CONFIGURATION_PATH=${CI_PRIMARY_REPOSITORY_PATH}/Configuration/Production/Config.xcconfig
 DEBUG_CONFIGURATION_PATH=${CI_PRIMARY_REPOSITORY_PATH}/Configuration/Debug/ConfigDebug.xcconfig
