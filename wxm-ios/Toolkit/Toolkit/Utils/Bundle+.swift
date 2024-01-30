438fa2b6b1854787b754ced40fab7b650905ca51
Pantelis Giazitsis <pgiazitsis@weatherxm.com>
2024-01-30 15:51:01 +0200

    Show branch name if exists in settings

--- wxm-ios/Toolkit/Toolkit/Utils/Bundle+.swift
+++ wxm-ios/Toolkit/Toolkit/Utils/Bundle+.swift
@@ -14,6 +14,7 @@ public enum ConfigurationKey: String {
 	case claimTokenUrl = "ClaimTokenUrl"
 	case appStoreUrl = "AppStoreUrl"
 	case supportUrl = "SupportUrl"
+	case branchName = "BranchName"
 }
 
 public extension Bundle {
