c900c3766e0de331a3a25d89b0f9bcf3a5aa9051
Pantelis Giazitsis <pgiazitsis@weatherxm.com>
2024-01-30 13:22:41 +0200

    Cleanup post clone script

--- ci_scripts/ci_post_clone.sh
+++ ci_scripts/ci_post_clone.sh
@@ -1,5 +1,20 @@
 #!/bin/sh
 
+setupConfiguration(){
+	CONFIGURATION_PATH=$1
+	touch $CONFIGURATION_PATH
+
+	echo "MBXAccessToken = ${MAPBOX_ACCESS_TOKEN};" > $CONFIGURATION_PATH
+	echo "UserAccessTokenService = accessTokenService;" >> $CONFIGURATION_PATH
+	echo "UserRefreshTokenService = refreshTokenService;" >> $CONFIGURATION_PATH
+	echo "Account = weatherXM;" >> $CONFIGURATION_PATH
+	echo "TeamId = ${CI_TEAM_ID};" >> $CONFIGURATION_PATH
+	echo "ApiUrl = ${API_URL};" >> $CONFIGURATION_PATH
+	echo "ClaimTokenUrl = ${CLAIM_TOKEN_URL};" >> $CONFIGURATION_PATH
+	echo "AppStoreUrl = ${APP_STORE_URL};" >> $CONFIGURATION_PATH
+	echo "SupportUrl = ${SUPPORT_URL};" >> $CONFIGURATION_PATH
+}
+
 touch ~/.netrc
 
 echo "Create mapbox files"
@@ -7,37 +22,21 @@ echo "machine api.mapbox.com" > ~/.netrc
 echo "login mapbox" >> ~/.netrc
 echo "password ${MAPBOX_TOKEN}" >> ~/.netrc
 
+if [ "$CI_WORKFLOW" = "Firebase Beta" ];
+then
 echo "Install Firebase CLI"
 curl -Lo ./firebase-tools-macos https://firebase.tools/bin/macos/latest
 chmod +x ./firebase-tools-macos
 
-CONFIGURATION_PATH=${CI_PRIMARY_REPOSITORY_PATH}/Configuration/Production/Config.xcconfig
 DEBUG_CONFIGURATION_PATH=${CI_PRIMARY_REPOSITORY_PATH}/Configuration/Debug/ConfigDebug.xcconfig
+setupConfiguration $DEBUG_CONFIGURATION_PATH
+fi
 
-touch $CONFIGURATION_PATH
-
-echo "MBXAccessToken = ${MAPBOX_ACCESS_TOKEN};" > $CONFIGURATION_PATH
-echo "UserAccessTokenService = accessTokenService;" >> $CONFIGURATION_PATH
-echo "UserRefreshTokenService = refreshTokenService;" >> $CONFIGURATION_PATH
-echo "Account = weatherXM;" >> $CONFIGURATION_PATH
-echo "TeamId = ${CI_TEAM_ID};" >> $CONFIGURATION_PATH
-echo "ApiUrl = ${API_URL};" >> $CONFIGURATION_PATH
-echo "ClaimTokenUrl = ${CLAIM_TOKEN_URL};" >> $CONFIGURATION_PATH
-echo "AppStoreUrl = ${APP_STORE_URL};" >> $CONFIGURATION_PATH
-echo "SupportUrl = ${SUPPORT_URL};" >> $CONFIGURATION_PATH
-
-
-touch $DEBUG_CONFIGURATION_PATH
-
-echo "MBXAccessToken = ${MAPBOX_ACCESS_TOKEN};" > $DEBUG_CONFIGURATION_PATH
-echo "UserAccessTokenService = accessTokenService;" >> $DEBUG_CONFIGURATION_PATH
-echo "UserRefreshTokenService = refreshTokenService;" >> $DEBUG_CONFIGURATION_PATH
-echo "Account = weatherXM;" >> $DEBUG_CONFIGURATION_PATH
-echo "TeamId = ${CI_TEAM_ID};" >> $DEBUG_CONFIGURATION_PATH
-echo "ApiUrl = ${API_URL};" >> $DEBUG_CONFIGURATION_PATH
-echo "ClaimTokenUrl = ${CLAIM_TOKEN_URL};" >> $DEBUG_CONFIGURATION_PATH
-echo "AppStoreUrl = ${APP_STORE_URL};" >> $DEBUG_CONFIGURATION_PATH
-echo "SupportUrl = ${SUPPORT_URL};" >> $DEBUG_CONFIGURATION_PATH
+if [ "$CI_WORKFLOW" = "Submit to App Store" ];
+then
+CONFIGURATION_PATH=${CI_PRIMARY_REPOSITORY_PATH}/Configuration/Production/Config.xcconfig
+setupConfiguration $CONFIGURATION_PATH
+fi
 
 defaults write com.apple.dt.Xcode IDESkipPackagePluginFingerprintValidatation -bool YES
 
@@ -66,15 +65,3 @@ echo ${GOOGLE_SERVICE_INFO_INTENT_DEBUG} | base64 -d > $INTENT_GOOGLE_SERVICES_D
 INTENT_GOOGLE_SERVICES_RELEASE_PATH=${CI_PRIMARY_REPOSITORY_PATH}/station-intent/Resources/Release
 mkdir -p $INTENT_GOOGLE_SERVICES_RELEASE_PATH
 echo ${GOOGLE_SERVICE_INFO_INTENT_RELEASE} | base64 -d > $INTENT_GOOGLE_SERVICES_RELEASE_PATH/GoogleService-Info.plist
-
-ls
-
-realpath $WXM_GOOGLE_SERVICES_DEBUG_PATH
-realpath $WXM_GOOGLE_SERVICES_RELEASE_PATH
-echo "\n"
-realpath $WIDGET_GOOGLE_SERVICES_DEBUG_PATH
-realpath $WIDGET_GOOGLE_SERVICES_RELEASE_PATH
-echo "\n"
-realpath $INTENT_GOOGLE_SERVICES_DEBUG_PATH
-realpath $INTENT_GOOGLE_SERVICES_RELEASE_PATH
-echo "\n"
