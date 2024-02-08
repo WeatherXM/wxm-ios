5aff2da4a38b8c3130e264cf46d119ed136ffc75
Pantelis Giazitsis <pgiazitsis@weatherxm.com>
2024-02-08 12:00:20 +0200

    fixup! Added reward annotation model

--- wxm-ios/DomainLayer/DomainLayer/Entities/DomainModels/RewardAnnotation.swift
+++ wxm-ios/DomainLayer/DomainLayer/Entities/DomainModels/RewardAnnotation.swift
@@ -6,3 +6,38 @@
 //
 
 import Foundation
+
+public struct RewardAnnotation: Codable {
+	public let severity: Severity?
+	public let group: Group?
+	public let title: String?
+	public let message: String?
+	public let docUrl: String?
+
+	enum CodingKeys: String, CodingKey {
+		case severity
+		case group
+		case title
+		case message
+		case docUrl = "doc_url"
+	}
+}
+
+public extension RewardAnnotation {
+	enum Severity: Codable {
+		case info
+		case warning
+		case error
+	}
+
+	enum Group: String, Codable {
+		case noWallet = "NO_WALLET"
+		case noStationData = "NO_STATION_DATA"
+		case sensorProblems = "SENSOR_PROBLEMS"
+		case weatherDataGaps = "WEATHER_DATA_GAPS"
+		case badStationDeployment = "BAD_STATION_DEPLOYMENT"
+		case noLocationData = "NO_LOCATION_DATA"
+		case locationNotVerified = "LOCATION_NOT_VERIFIED"
+		case userRelocationPenalty = "USER_RELOCATION_PENALTY"
+	}
+}
