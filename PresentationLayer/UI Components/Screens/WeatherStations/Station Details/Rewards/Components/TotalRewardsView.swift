5522d6fabf3a031539dc5e75366a49d85ca5dc04
Pantelis Giazitsis <pgiazitsis@weatherxm.com>
2024-02-16 12:11:51 +0200

    Total rewards card

--- PresentationLayer/UI Components/Screens/WeatherStations/Station Details/Rewards/Components/TotalRewardsView.swift
+++ PresentationLayer/UI Components/Screens/WeatherStations/Station Details/Rewards/Components/TotalRewardsView.swift
@@ -8,11 +8,33 @@
 import SwiftUI
 
 struct TotalRewardsView: View {
+	let rewards: Double
+
     var body: some View {
-		Image(asset: .wxmBlackLogo)
+		HStack(spacing: CGFloat(.defaultSpacing)) {
+			Image(asset: .wxmBlackLogo)
+
+			VStack(spacing: 0.0) {
+				HStack {
+					Text(LocalizableString.StationDetails.rewardsTitle.localized)
+						.font(.system(size: CGFloat(.normalFontSize)))
+						.foregroundColor(Color(colorEnum: .darkGrey))
+					Spacer()
+				}
+
+				HStack {
+					Text("\(rewards, specifier: "%.2f") \(StringConstants.wxmCurrency)")
+						.font(.system(size: CGFloat(.titleFontSize), weight: .bold))
+						.foregroundColor(Color(colorEnum: .text))
+					Spacer()
+				}
+			}
+		}
+		.WXMCardStyle()
     }
 }
 
 #Preview {
-    TotalRewardsView()
+    TotalRewardsView(rewards: 14501234)
+		.wxmShadow()
 }
