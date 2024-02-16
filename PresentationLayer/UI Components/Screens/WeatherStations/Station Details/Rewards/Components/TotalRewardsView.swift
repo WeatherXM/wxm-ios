9829eef966e7094d07cb6f9659106b3fcbc6e418
Pantelis Giazitsis <pgiazitsis@weatherxm.com>
2024-02-16 11:17:11 +0200

    Added total rewards view

---
+++ PresentationLayer/UI Components/Screens/WeatherStations/Station Details/Rewards/Components/TotalRewardsView.swift
@@ -0,0 +1,18 @@
+//
+//  TotalRewardsView.swift
+//  wxm-ios
+//
+//  Created by Pantelis Giazitsis on 16/2/24.
+//
+
+import SwiftUI
+
+struct TotalRewardsView: View {
+    var body: some View {
+		Image(asset: .wxmBlackLogo)
+    }
+}
+
+#Preview {
+    TotalRewardsView()
+}
