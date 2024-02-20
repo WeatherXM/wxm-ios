ee4d0278d5be2f55e0c7a06180307136cfb2134a
Pantelis Giazitsis <pgiazitsis@weatherxm.com>
2024-02-20 16:10:51 +0200

    Cleanup

--- wxm-ios/DomainLayer/DomainLayer/UseCases/RewardsUseCase.swift
+++ wxm-ios/DomainLayer/DomainLayer/UseCases/RewardsUseCase.swift
@@ -22,34 +22,6 @@ public struct RewardsUseCase {
         self.keychainRepository = keychainRepository
     }
 
-    public func getDeviceTokenTransactions(deviceId: String,
-                                    page: Int,
-                                    pageSize: Int, timezone: String,
-                                    fromDate: String,
-                                    toDate: String) async throws -> Result<NetworkDeviceIDTokensTransactionsResponse, NetworkErrorResponse> {
-        let isUserDeviceResult = try await meRepository.getDeviceFollowState(deviceId: deviceId)
-        switch isUserDeviceResult {
-            case .success(let isUserDevice):
-                if let isUserDevice {
-                    return try await getUserDeviceTokensTransactions(deviceId: deviceId,
-                                                                     page: page,
-                                                                     pageSize: pageSize,
-                                                                     timezone: timezone,
-                                                                     fromDate: fromDate,
-                                                                     toDate: toDate)
-                }
-
-                return try await getPublicDeviceTokenTransactions(deviceId: deviceId,
-                                                                  page: page,
-                                                                  pageSize: pageSize,
-                                                                  timezone: timezone,
-                                                                  fromDate: fromDate,
-                                                                  toDate: toDate)
-            case .failure(let error):
-                return .failure(error)
-        }
-    }
-
 	#warning("TODO: - Remove once the reward summary is implemented")
 	public func getDeviceRewards(deviceId: String) async throws -> Result<NetworkDeviceTokensResponse, NetworkErrorResponse> {
 		let rewardsPublisher = try devicesRepository.deviceRewards(deviceId: deviceId)
@@ -76,54 +48,4 @@ public struct RewardsUseCase {
 			}.store(in: &cancellables.cancellableSet)
 		}
 	}
-}
-
-private extension RewardsUseCase {
-
-    func getUserDeviceTokensTransactions(deviceId: String,
-                                         page: Int,
-                                         pageSize: Int, timezone: String,
-                                         fromDate: String,
-                                         toDate: String) async throws -> Result<NetworkDeviceIDTokensTransactionsResponse, NetworkErrorResponse> {
-        let userDeviceTransactions = try meRepository.getUserDeviceTokensTransactionsById(deviceId: deviceId,
-                                                                                          page: page,
-                                                                                          pageSize: pageSize,
-                                                                                          timezone: timezone,
-                                                                                          fromDate: fromDate,
-                                                                                          toDate: toDate)
-        return await withCheckedContinuation { continuation in
-            userDeviceTransactions.sink { response in
-                if let error = response.error {
-                    continuation.resume(returning: .failure(error))
-                } else {
-                    continuation.resume(returning: .success(response.value!))
-                }
-            }
-            .store(in: &cancellables.cancellableSet)
-        }
-    }
-
-    func getPublicDeviceTokenTransactions(deviceId: String,
-                                          page: Int,
-                                          pageSize: Int, timezone: String,
-                                          fromDate: String,
-                                          toDate: String) async throws -> Result<NetworkDeviceIDTokensTransactionsResponse, NetworkErrorResponse> {
-        let deviceTransactions = try devicesRepository.deviceTokenTransactions(deviceId: deviceId,
-                                                                               page: page,
-                                                                               pageSize: pageSize,
-                                                                               timezone: timezone,
-                                                                               fromDate: fromDate,
-                                                                               toDate: toDate)
-
-        return await withCheckedContinuation { continuation in
-            deviceTransactions.sink { response in
-                if let error = response.error {
-                    continuation.resume(returning: .failure(error))
-                } else {
-                    continuation.resume(returning: .success(response.value!))
-                }
-            }
-            .store(in: &cancellables.cancellableSet)
-        }
-    }
 }
