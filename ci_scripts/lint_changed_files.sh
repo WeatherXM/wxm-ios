ba8cae2db5d0c19196b619bf815d28c06c22320b
Pantelis Giazitsis <pgiazitsis@weatherxm.com>
2024-02-26 12:25:48 +0200

    fixup! Functionality in script

--- ci_scripts/lint_changed_files.sh
+++ ci_scripts/lint_changed_files.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
-git diff --name-only main..HEAD \
-          | grep '\.swift$' \
-          | xargs -I% swiftlint lint --strict --quiet  codeclimate "%" | sed -E 's/^(.*):([0-9]+):([0-9]+): (warning|error|[^:]+): (.*)/::\4 title=Lint error,file=\1,line=\2,col=\3::\5\n\1:\2:\3/'
+changedFiles=$(git diff --name-only main..HEAD | grep '\.swift$')
+echo $changedFiles
+changedFiles  | xargs -I% swiftlint lint --strict --quiet  codeclimate "%" | sed -E 's/^(.*):([0-9]+):([0-9]+): (warning|error|[^:]+): (.*)/::\4 title=Lint error,file=\1,line=\2,col=\3::\5\n\1:\2:\3/'
