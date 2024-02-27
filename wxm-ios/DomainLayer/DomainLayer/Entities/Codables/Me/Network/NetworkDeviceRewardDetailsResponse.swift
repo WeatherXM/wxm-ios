fa1a2c0db8f2d452e5806b17683f6fdd3cc7810c
Pantelis Giazitsis <pgiazitsis@weatherxm.com>
2024-02-27 16:40:08 +0200

    Added details backend stuff

--- wxm-ios/DomainLayer/DomainLayer/Entities/Codables/Me/Network/NetworkDeviceRewardDetailsResponse.swift
+++ wxm-ios/DomainLayer/DomainLayer/Entities/Codables/Me/Network/NetworkDeviceRewardDetailsResponse.swift
@@ -9,4 +9,46 @@ import Foundation
 
 public struct NetworkDeviceRewardDetailsResponse: Codable {
 	public let totalDailyReward: Double?
+	public let base: Base?
+	public let boost: Boost?
+}
+
+public extension NetworkDeviceRewardDetailsResponse {
+	struct Base: Codable {
+		let actualReward: Double?
+		let rewardScore: Int?
+		let maxReward: Double?
+
+		enum CodingKeys: String, CodingKey {
+			case actualReward = "actual_reward"
+			case rewardScore = "reward_score"
+			case maxReward = "max_reward"
+		}
+	}
+
+	struct Boost: Codable {
+		let totalReward: Double?
+		let data: [BoostReward]?
+	}
+
+	struct BoostReward: Codable {
+		let title: String?
+		let description: String?
+		let actualReward: Double?
+		let rewardScore: Int?
+		let maxReward: Double?
+
+		enum CodingKeys: String, CodingKey {
+			case title
+			case description
+			case actualReward = "actual_reward"
+			case rewardScore = "reward_score"
+			case maxReward = "max_reward"
+		}
+	}
+
+	struct Annotation: Codable {
+		let score: Int?
+		let summary: [NetworkDeviceRewardsSummary]?
+	}
 }
