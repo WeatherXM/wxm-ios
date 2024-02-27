1350289a1f14b4841d92e95423969679b691b573
Pantelis Giazitsis <pgiazitsis@weatherxm.com>
2024-02-27 14:19:28 +0200

    Added model

---
+++ wxm-ios/DomainLayer/DomainLayer/Entities/Codables/Me/Network/NetworkDeviceRewardDetailsResponse.swift
@@ -0,0 +1,12 @@
+//
+//  NetworkDeviceRewardDetailsResponse.swift
+//  DomainLayer
+//
+//  Created by Pantelis Giazitsis on 27/2/24.
+//
+
+import Foundation
+
+public struct NetworkDeviceRewardDetailsResponse: Codable {
+	public let totalDailyReward: Double?
+}
