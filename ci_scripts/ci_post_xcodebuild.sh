5b9c42828ddd40dfe08f0c4c503b716c3e94ab9b
Pantelis Giazitsis <pgiazitsis@weatherxm.com>
2024-04-24 16:43:12 +0300

    Upload with params

--- ci_scripts/ci_post_xcodebuild.sh
+++ ci_scripts/ci_post_xcodebuild.sh
@@ -1,11 +1,13 @@
 #!/bin/sh
 
 uploadDSyms(){
+	CONFIGURATION=$1
+
 	UPLOAD_SYMBOLS=$CI_DERIVED_DATA_PATH/SourcePackages/checkouts/firebase-ios-sdk/Crashlytics/upload-symbols
 
-	APP_GOOGLE_SERVICES_PATH=${CI_PRIMARY_REPOSITORY_PATH}/wxm-ios/Resources/Debug/GoogleService-Info.plist
-	WIDGET_GOOGLE_SERVICES_PATH=${CI_PRIMARY_REPOSITORY_PATH}/station-widget/Resources/Debug/GoogleService-Info.plist
-	INTENT_GOOGLE_SERVICES_PATH=${CI_PRIMARY_REPOSITORY_PATH}/station-intent/Resources/Debug/GoogleService-Info.plist
+	APP_GOOGLE_SERVICES_PATH=${CI_PRIMARY_REPOSITORY_PATH}/wxm-ios/Resources/$CONFIGURATION/GoogleService-Info.plist
+	WIDGET_GOOGLE_SERVICES_PATH=${CI_PRIMARY_REPOSITORY_PATH}/station-widget/Resources/$CONFIGURATION/GoogleService-Info.plist
+	INTENT_GOOGLE_SERVICES_PATH=${CI_PRIMARY_REPOSITORY_PATH}/station-intent/Resources/$CONFIGURATION/GoogleService-Info.plist
 
 	ZIP_DSYMS=./dsyms.zip
 	zip -r $ZIP_DSYMS $CI_ARCHIVE_PATH/dSYMs
@@ -18,12 +20,16 @@ uploadDSyms(){
 if ([ "$CI_WORKFLOW" = "QA Production" ]) || ([ "$CI_WORKFLOW" = "QA Dev" ]);
 then
 sh ./firebase_submission.sh -p ${CI_AD_HOC_SIGNED_APP_PATH}/WeatherXM.ipa -k ${FIREBASE_DEBUG_APP_ID} -t ${FIREBASE_REFRESH_TOKEN} -g "qa-group"
-uploadDSyms
+uploadDSyms Debug
 fi
 
 if ([ "$CI_WORKFLOW" = "Dev Build" ])
 then
 sh ./firebase_submission.sh -p ${CI_AD_HOC_SIGNED_APP_PATH}/WeatherXM.ipa -k ${FIREBASE_DEBUG_APP_ID} -t ${FIREBASE_REFRESH_TOKEN} -g "tech-team"
-uploadDSyms
+uploadDSyms Debug
 fi
 
+if ([ "$CI_WORKFLOW" = "Submit to App Store" ])
+uploadDSyms Release
+then
+fi
