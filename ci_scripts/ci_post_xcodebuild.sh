52d5d037d52be87356dae6e306010271b1287a59
Pantelis Giazitsis <pgiazitsis@weatherxm.com>
2024-04-24 16:08:26 +0300

    Upload dsyms

--- ci_scripts/ci_post_xcodebuild.sh
+++ ci_scripts/ci_post_xcodebuild.sh
@@ -1,13 +1,29 @@
 #!/bin/sh
-ls $CI_DERIVED_DATA_PATH
-#if ([ "$CI_WORKFLOW" = "QA Production" ]) || ([ "$CI_WORKFLOW" = "QA Dev" ]);
-#then
-#sh ./firebase_submission.sh -p ${CI_AD_HOC_SIGNED_APP_PATH}/WeatherXM.ipa -k ${FIREBASE_DEBUG_APP_ID} -t ${FIREBASE_REFRESH_TOKEN} -g "qa-group"
-#fi
-#
-#if ([ "$CI_WORKFLOW" = "Dev Build" ])
-#then
-#sh ./firebase_submission.sh -p ${CI_AD_HOC_SIGNED_APP_PATH}/WeatherXM.ipa -k ${FIREBASE_DEBUG_APP_ID} -t ${FIREBASE_REFRESH_TOKEN} -g "tech-team"
-#fi
 
+uploadDSyms(){
+	UPLOAD_SYMBOLS=$CI_DERIVED_DATA_PATH/SourcePackages/checkouts/firebase-ios-sdk/Crashlytics/upload-symbols
+
+	APP_GOOGLE_SERVICES_PATH=${CI_PRIMARY_REPOSITORY_PATH}/wxm-ios/Resources/Debug/GoogleService-Info.plist
+	WIDGET_GOOGLE_SERVICES_PATH=${CI_PRIMARY_REPOSITORY_PATH}/station-widget/Resources/Debug/GoogleService-Info.plist
+	INTENT_GOOGLE_SERVICES_PATH=${CI_PRIMARY_REPOSITORY_PATH}/station-intent/Resources/Debug/GoogleService-Info.plist
+
+	ZIP_DSYMS=./dsyms.zip
+	zip -r $ZIP_DSYMS $CI_ARCHIVE_PATH/dSYMs
+
+	eval $UPLOAD_SYMBOLS -gsp $APP_GOOGLE_SERVICES_PATH -p ios ./dsyms.zip
+	eval $UPLOAD_SYMBOLS -gsp $WIDGET_GOOGLE_SERVICES_PATH -p ios ./dsyms.zip
+	eval $UPLOAD_SYMBOLS -gsp $INTENT_GOOGLE_SERVICES_PATH -p ios ./dsyms.zip
+}
+
+if ([ "$CI_WORKFLOW" = "QA Production" ]) || ([ "$CI_WORKFLOW" = "QA Dev" ]);
+then
+sh ./firebase_submission.sh -p ${CI_AD_HOC_SIGNED_APP_PATH}/WeatherXM.ipa -k ${FIREBASE_DEBUG_APP_ID} -t ${FIREBASE_REFRESH_TOKEN} -g "qa-group"
+uploadDSyms
+fi
+
+if ([ "$CI_WORKFLOW" = "Dev Build" ])
+then
+sh ./firebase_submission.sh -p ${CI_AD_HOC_SIGNED_APP_PATH}/WeatherXM.ipa -k ${FIREBASE_DEBUG_APP_ID} -t ${FIREBASE_REFRESH_TOKEN} -g "tech-team"
+uploadDSyms
+fi
 
