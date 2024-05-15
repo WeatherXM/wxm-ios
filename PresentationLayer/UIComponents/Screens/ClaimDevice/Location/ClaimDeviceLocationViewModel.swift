8de95b38b4a085b0c36cc6767fd07db453db1880
Pantelis Giazitsis <pgiazitsis@weatherxm.com>
2024-05-15 18:09:21 +0300

    Added files

---
+++ PresentationLayer/UIComponents/Screens/ClaimDevice/Location/ClaimDeviceLocationViewModel.swift
@@ -0,0 +1,12 @@
+//
+//  ClaimDeviceLocationViewModel.swift
+//  wxm-ios
+//
+//  Created by Pantelis Giazitsis on 15/5/24.
+//
+
+import Foundation
+
+class ClaimDeviceLocationViewModel: ObservableObject {
+	
+}
