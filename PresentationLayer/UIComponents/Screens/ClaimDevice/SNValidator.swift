ee8e79f4dede58ef319ce7ae4e95d195e5751e92
Pantelis Giazitsis <pgiazitsis@weatherxm.com>
2024-05-17 12:24:25 +0300

    Fixed D1 regex

--- PresentationLayer/UIComponents/Screens/ClaimDevice/SNValidator.swift
+++ PresentationLayer/UIComponents/Screens/ClaimDevice/SNValidator.swift
@@ -9,7 +9,7 @@ import Foundation
 
 struct SNValidator {
 	private let m5SerialNumberRegex = "^([0-9A-Fa-f]{2}:){8}[0-9A-Fa-f]{2}"
-	private let d1SerialNumberRegex = "^([0-9A-Fa-f]{2}:){10}[0-9A-Fa-f]{2}"
+	private let d1SerialNumberRegex = "^([0-9A-Fa-f]{2}:){9}[0-9A-Fa-f]{2}"
 	private let claimingKeyRegex = "^([0-9]{6})"
 
 	func validateM5(serialNumber: String) -> Bool {
