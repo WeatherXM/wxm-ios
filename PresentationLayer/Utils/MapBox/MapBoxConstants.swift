1241fef0c7895fa6f6459ed6c0debe5b458bda9a
Pantelis Giazitsis <pgiazitsis@weatherxm.com>
2024-06-12 15:07:13 +0300

    Inject mapbox style from config

--- PresentationLayer/Utils/MapBox/MapBoxConstants.swift
+++ PresentationLayer/Utils/MapBox/MapBoxConstants.swift
@@ -9,7 +9,7 @@ import Foundation
 import UIKit
 
 enum MapBoxConstants {
-	static let mapBoxStyle = "mapbox://styles/exmachina/ckrxjh01a5e7317plznjeicao"
+	static let mapBoxStyle = Bundle.main.getConfiguration(for: .mapBoxStyle) ?? ""
 	static let polygonFillColor = UIColor(red: 51.0/255.0, green: 136.0/255.0, blue: 255.0/255.0, alpha: 0.5)
 	static let snapshotSize: CGSize = CGSize(width: 340.0, height: 200.0)
 	static let snapshotZoom: CGFloat = 11.0
