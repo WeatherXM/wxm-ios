1eb5897ae046f4d33873610e7cc2d785acbade22
Pantelis Giazitsis <giazitsispantelis@gmail.com>
2024-06-17 15:08:48 +0300

    Add new region AS923_1b and map to malaysia (#95)
    * Added new frequency
    
    * Updated frequency for Malaysia
    
    ---------
    
    Co-authored-by: Pantelis Giazitsis <pgiazitsis@weatherxm.com>

--- wxm-ios/DomainLayer/DomainLayer/Entities/DomainModels/Frequency.swift
+++ wxm-ios/DomainLayer/DomainLayer/Entities/DomainModels/Frequency.swift
@@ -14,6 +14,7 @@ public enum Frequency: String, CaseIterable, Identifiable, Equatable, Codable {
     case IN865
     case RU864
     case AS923_1
+	case AS923_1B
     case AS923_2
     case AS923_3
     case AS923_4
@@ -23,30 +24,32 @@ public enum Frequency: String, CaseIterable, Identifiable, Equatable, Codable {
 }
 
 public extension Frequency {
-    var heliumBand: UInt {
-        switch self {
-        case .EU868:
-            return 5
-        case .US915:
-            return 8
-        case .AU915:
-            return 1
-        case .CN470:
-            return 2
-        case .KR920:
-            return 6
-        case .IN865:
-            return 7
-        case .RU864:
-            return 9
-        case .AS923_1:
-            return 10
-        case .AS923_2:
-            return 11
-        case .AS923_3:
-            return 12
-        case .AS923_4:
-            return 13
-        }
-    }
+	var heliumBand: UInt {
+		switch self {
+			case .EU868:
+				return 5
+			case .US915:
+				return 8
+			case .AU915:
+				return 1
+			case .CN470:
+				return 2
+			case .KR920:
+				return 6
+			case .IN865:
+				return 7
+			case .RU864:
+				return 9
+			case .AS923_1:
+				return 10
+			case .AS923_2:
+				return 11
+			case .AS923_3:
+				return 12
+			case .AS923_4:
+				return 13
+			case .AS923_1B:
+				return 14
+		}
+	}
 }
