6e7957e6af1af1b68686e734b8a2f19ea4035e6c
Pantelis Giazitsis <pgiazitsis@weatherxm.com>
2024-07-04 14:19:45 +0300

    Fixed format for forecast date string

--- wxm-ios/Toolkit/Toolkit/Utils/DateExtension.swift
+++ wxm-ios/Toolkit/Toolkit/Utils/DateExtension.swift
@@ -20,7 +20,7 @@ public extension Date {
 		case monthLiteralDayTime = "MMM d, HH:mm"
         case dayFullLiteral = "EEEE"
         case dayShortLiteralDayMonth = "E dd, MMM yy"
-		case dayShortLiteralMonthDay = "E, MMM yy"
+		case dayShortLiteralMonthDay = "E, MMM d"
         case fullDateTime = "MM/dd/yyyy, HH:mm"
     }
 
