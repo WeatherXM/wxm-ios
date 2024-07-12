1d0e58556a5cd049074ae4ae61850aca560dd285
Pantelis Giazitsis <pgiazitsis@weatherxm.com>
2024-07-12 12:26:54 +0300

    WIP

---
+++ WeatherXMTests/KeychainUseCaseTests.swift
@@ -0,0 +1,42 @@
+//
+//  KeychainUseCaseTests.swift
+//  WeatherXMTests
+//
+//  Created by Pantelis Giazitsis on 11/7/24.
+//
+
+import XCTest
+import Cuckoo
+@testable import DomainLayer
+
+final class KeychainUseCaseTests: XCTestCase {
+
+
+
+//	let useCase: KeychainUseCase = .init(keychainRepository: <#T##any KeychainRepository#>)
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+//	let pro = Mockrew
+	
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+        // Any test you write for XCTest can be annotated as throws and async.
+        // Mark your test throws to produce an unexpected failure when your test encounters an uncaught error.
+        // Mark your test async to allow awaiting for asynchronous code to complete. Check the results with assertions afterwards.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
