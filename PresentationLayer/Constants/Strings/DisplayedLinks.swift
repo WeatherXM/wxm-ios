ba5cb8747f59f0b74b84bdc234375bc55990db6a
Pantelis Giazitsis <giazitsispantelis@gmail.com>
2024-08-22 10:25:23 +0300

    Replace docs urls (#134)
    * Updated create wallet link
    
    * Updated troubleshooting links
    
    * Updated reward mechanism link
    
    * Updated cell capacity link
    
    * Updated pol and qod links
    
    * Updated battery links
    
    * Updated app groups
    
    ---------
    
    Co-authored-by: Pantelis Giazitsis <pgiazitsis@weatherxm.com>

--- PresentationLayer/Constants/Strings/DisplayedLinks.swift
+++ PresentationLayer/Constants/Strings/DisplayedLinks.swift
@@ -14,7 +14,7 @@ enum DisplayedLinks {
 	case termsLink
 	case contactLink
 	case weatherXMWebsiteLink
-	case readMoreAboutWalletsLink
+	case createWalletsLink
 	case arbitrumAddressWebsiteFormat
 	case documentationLink
 	case feedbackForm
@@ -50,8 +50,8 @@ enum DisplayedLinks {
 				return "https://weatherxm.com/contact/"
 			case .weatherXMWebsiteLink:
 				return "https://weatherxm.com/"
-			case .readMoreAboutWalletsLink:
-				return "https://docs.weatherxm.com/wallet/add-edit-wallet-address#how-to-create-wallet-on-metamask"
+			case .createWalletsLink:
+				return "https://docs.weatherxm.com/mobile-app/wallet/add-edit-wallet-address#how-to-create-wallet-on-metamask"
 			case .arbitrumAddressWebsiteFormat:
 				return "https://arbiscan.io/address/%@#tokentxns"
 			case .documentationLink:
@@ -61,11 +61,11 @@ enum DisplayedLinks {
 			case .appSurveyForm:
 				return "https://docs.google.com/forms/d/e/1FAIpQLSc1T-odUwpXa01nODavwEtLgO00AfPxqfrdhbn_G5Lss0dD2w/viewform?usp=pp_url"
 			case .m5Troubleshooting:
-				return "https://docs.weatherxm.com/wifi-m5-bundle/m5-troubleshooting"
+				return "https://docs.weatherxm.com/wxm-devices/m5/troubleshooting"
 			case .heliumTroubleshooting:
-				return "https://docs.weatherxm.com/helium-bundle/helium-troubleshooting"
+				return "https://docs.weatherxm.com/wxm-devices/helium/troubleshooting"
 			case .d1Troubleshooting:
-				return "https://docs.weatherxm.com/wxm-devices/D1-bundle/D1-troubleshooting"
+				return "https://docs.weatherxm.com/wxm-devices/d1/troubleshooting"
 			case .heliumRegionFrequencies:
 				return "https://docs.helium.com/iot/lorawan-region-plans"
 			case .shopLink:
@@ -75,15 +75,15 @@ enum DisplayedLinks {
 			case .shareCells:
 				return "https://explorer.weatherxm.com/cells/"
 			case .rewardMechanism:
-				return "https://docs.weatherxm.com/reward-mechanism"
+				return "https://docs.weatherxm.com/rewards/reward-mechanism"
 			case .polAlgorithm:
-				return "https://docs.weatherxm.com/project/proof-of-location"
+				return "https://docs.weatherxm.com/rewards/proof-of-location"
 			case .qodAlgorithm:
-				return "https://docs.weatherxm.com/project/quality-of-data"
+				return "https://docs.weatherxm.com/rewards/quality-of-data"
 			case .troubleshooting:
 				return "https://docs.weatherxm.com/faqs#troubleshooting"
 			case .cellCapacity:
-				return "https://docs.weatherxm.com/project/cell-capacity"
+				return "https://docs.weatherxm.com/rewards/cell-capacity"
 			case .claimToken:
 				return Bundle.main.getConfiguration(for: .claimTokenUrl) ?? ""
 			case .appstore:
@@ -91,11 +91,11 @@ enum DisplayedLinks {
 			case .announcements:
 				return "https://announcements.weatherxm.com"
 			case .m5Batteries:
-				return "https://docs.weatherxm.com/wxm-devices/wifi-m5-bundle/assemble-m5#installing-batteries"
+				return "https://docs.weatherxm.com/wxm-devices/m5/assemble#installing-batteries"
 			case .heliumBatteries:
-				return "https://docs.weatherxm.com/wxm-devices/helium-bundle/helium-assemble#battery-installation-diagram-external-box"
+				return "https://docs.weatherxm.com/wxm-devices/helium/assemble#battery-installation-diagram-external-box"
 			case .d1Batteries:
-				return "https://docs.weatherxm.com/wxm-devices/D1-bundle/D1-assemble#installing-batteries"
+				return "https://docs.weatherxm.com/wxm-devices/d1/assemble#installing-batteries"
 			case .m5VideoLink:
 				return "https://www.youtube.com/watch?v=sUJEwuFq1CE"
 			case .d1VideoLink:
