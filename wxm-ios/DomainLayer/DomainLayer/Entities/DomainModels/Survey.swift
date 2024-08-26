70083878e5b04deffc5ee0acc374c3bf74f7683f
Pantelis Giazitsis <pgiazitsis@weatherxm.com>
2024-08-26 13:56:12 +0300

    Added survey models

---
+++ wxm-ios/DomainLayer/DomainLayer/Entities/DomainModels/Survey.swift
@@ -0,0 +1,16 @@
+//
+//  Survey.swift
+//  DomainLayer
+//
+//  Created by Pantelis Giazitsis on 26/8/24.
+//
+
+import Foundation
+
+public struct Survey {
+	public let id: String?
+	public let title: String?
+	public let message: String?
+	public let actionLabel: String?
+	public let Url: String?
+}
---
+++ wxm-ios/DomainLayer/DomainLayer/UseCases/SurveyUseCase.swift
@@ -0,0 +1,11 @@
+//
+//  SurveyUseCase.swift
+//  DomainLayer
+//
+//  Created by Pantelis Giazitsis on 26/8/24.
+//
+
+import Foundation
+
+public struct SurveyUseCase {
+}
