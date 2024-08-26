0467676355e31c2efa94e7e56041263a5994e338
Pantelis Giazitsis <pgiazitsis@weatherxm.com>
2024-08-26 16:06:12 +0300

    Updated announcements view

--- PresentationLayer/UIComponents/Screens/WeatherStations/StationDetails/Rewards/Components/AnnouncementCardView.swift
+++ PresentationLayer/UIComponents/Screens/WeatherStations/StationDetails/Rewards/Components/AnnouncementCardView.swift
@@ -6,22 +6,19 @@
 //
 
 import SwiftUI
+import Toolkit
 
 struct AnnouncementCardView: View {
 	let title: String
 	let description: String
-	
+	var actionTitle: String?
+	var action: VoidCallback?
+
     var body: some View {
 		VStack(spacing: CGFloat(.smallSpacing)) {
 			HStack(spacing: CGFloat(.smallSpacing)) {
-				Spacer()
-
-				Text(FontIcon.hexagonCheck.rawValue)
-					.font(.fontAwesome(font: .FAPro, size: CGFloat(.XLTitleFontSize)))
-					.foregroundColor(Color(colorEnum: .wxmWhite))
-
 				Text(title)
-					.font(.system(size: CGFloat(.XLTitleFontSize), weight: .bold))
+					.font(.system(size: CGFloat(.mediumFontSize), weight: .bold))
 					.foregroundColor(Color(colorEnum: .wxmWhite))
 					.lineLimit(1)
 
@@ -31,9 +28,33 @@ struct AnnouncementCardView: View {
 			.minimumScaleFactor(0.8)
 
 			Text(description)
-				.font(.system(size: CGFloat(.mediumFontSize)))
+				.font(.system(size: CGFloat(.normalFontSize)))
 				.foregroundColor(Color(colorEnum: .wxmWhite))
-				.multilineTextAlignment(.center)
+
+			if let actionTitle, let action {
+				HStack {
+					Button(action: action) {
+						HStack(spacing: CGFloat(.minimumSpacing)) {
+							Text(actionTitle)
+								.font(.system(size: CGFloat(.caption), weight: .bold))
+								.foregroundColor(Color(colorEnum: .wxmWhite))
+
+							Text(FontIcon.externalLink.rawValue)
+								.font(.fontAwesome(font: .FAProSolid,
+												   size: CGFloat(.caption)))
+								.foregroundColor(Color(colorEnum: .wxmWhite))
+
+						}
+						.padding(.horizontal, CGFloat(.mediumSidePadding))
+						.padding(.vertical, CGFloat(.smallSidePadding))
+						.background {
+							Capsule().foregroundStyle(Color(colorEnum: .wxmWhite).opacity(0.2))
+						}
+					}
+
+					Spacer()
+				}
+			}
 		}
 		.padding(CGFloat(.defaultSidePadding))
 		.background {
@@ -47,6 +68,7 @@ struct AnnouncementCardView: View {
 
 #Preview {
     AnnouncementCardView(title: "Welcome to Mainnet!",
-						 description: "Starting the Χth of Υ all station rewards are distributed on Abritrum Mainnet!\n\nThank you for the support!")
+						 description: "Starting the Χth of Υ all station rewards are distributed on Abritrum Mainnet!\n\nThank you for the support!",
+						 actionTitle: "Action title") { }
 	.padding(.horizontal, 30)
 }
