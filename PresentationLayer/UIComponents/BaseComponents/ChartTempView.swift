7ba4048732491b839b3cc8baacf788c7c4a92861
Pantelis Giazitsis <pgiazitsis@weatherxm.com>
2024-08-28 17:24:07 +0300

    Temp for testing

---
+++ PresentationLayer/UIComponents/BaseComponents/ChartTempView.swift
@@ -0,0 +1,18 @@
+//
+//  ChartTempView.swift
+//  wxm-ios
+//
+//  Created by Pantelis Giazitsis on 28/8/24.
+//
+
+import SwiftUI
+
+struct ChartTempView: View {
+    var body: some View {
+		Text(verbatim: "")
+    }
+}
+
+#Preview {
+    ChartTempView()
+}
