9877f97c380fa1014b5ef5be55d2c2aa330d8bf3
Pantelis Giazitsis <pgiazitsis@weatherxm.com>
2024-09-02 13:51:10 +0300

    Explorer data equatable performance

---
+++ PresentationLayer/Extensions/DomainExtensions/ExplorerData+.swift
@@ -0,0 +1,17 @@
+//
+//  ExplorerData+.swift
+//  wxm-ios
+//
+//  Created by Pantelis Giazitsis on 2/9/24.
+//
+
+import Foundation
+import DomainLayer
+import MapboxMaps
+
+extension ExplorerData: Equatable {
+	public static func == (lhs: ExplorerData, rhs: ExplorerData) -> Bool {
+		lhs.totalDevices == rhs.totalDevices &&
+		lhs.polygonPoints == rhs.polygonPoints
+	}
+}
