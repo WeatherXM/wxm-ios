b63d298cbc8dd4a599688f7c079b140421a77a75
Pantelis Giazitsis <pgiazitsis@weatherxm.com>
2024-10-11 12:24:58 +0300

    New layout in weather overview view

--- PresentationLayer/UIComponents/BaseComponents/PercentageGridLayoutView.swift
+++ PresentationLayer/UIComponents/BaseComponents/PercentageGridLayoutView.swift
@@ -10,25 +10,31 @@ import SwiftUI
 struct PercentageGridLayoutView<Content: View>: View {
     var alignments: [Alignment] = [.leading, .leading]
     var firstColumnPercentage: CGFloat = 0.6
+	var linesSpacing: CGFloat?
 
     let content: () -> Content
     @State private var size: CGSize = .zero
 
-    var body: some View {
-		#if MAIN_APP
-        LazyVGrid(columns: [GridItem(.fixed(size.width * firstColumnPercentage), spacing: 0.0, alignment: alignments[0]),
-                            GridItem(.flexible(), spacing: 0.0, alignment: alignments[1])]) {
-            content()
-        }.sizeObserver(size: $size)
-		#else
+	var body: some View {
+#if MAIN_APP
+		LazyVGrid(columns: [GridItem(.fixed(size.width * firstColumnPercentage),
+									 spacing: 0.0,
+									 alignment: alignments[0]),
+							GridItem(.flexible(),
+									 spacing: 0.0,
+									 alignment: alignments[1])],
+				  spacing: linesSpacing) {
+			content()
+		}.sizeObserver(size: $size)
+#else
 		VStack {
 			LazyVGrid(columns: [GridItem(.flexible(), spacing: 0.0, alignment: alignments[0]),
 								GridItem(.flexible(), spacing: 0.0, alignment: alignments[1])]) {
 				content()
 			}
 		}
-		#endif
-    }
+#endif
+	}
 }
 
 struct PercentageGridLayoutView_Previews: PreviewProvider {
