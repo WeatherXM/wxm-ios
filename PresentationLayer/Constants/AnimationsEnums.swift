bc7b18c77739066e8171d97de4812d7207e9ffe0
Pantelis Giazitsis <pgiazitsis@weatherxm.com>
2024-11-04 10:43:27 +0200

    Renamed files

--- PresentationLayer/Constants/AnimationsEnums.swift
+++ PresentationLayer/Constants/AnimationsEnums.swift
@@ -71,19 +71,19 @@ enum AnimationsEnums: String, CaseIterable {
 			case .notAvailable:
 				return "not_available"
 			case .clearDay:
-				return "weather_clear-day"
+				return "weather_clear_day"
 			case .clearNight:
-				return "weather_clear-night"
+				return "weather_clear_night"
 			case .partlyCloudyDay:
-				return "weather_partly-cloudy-day"
+				return "weather_partly_cloudy_day"
 			case .partlyCloudyNight:
-				return "weather_partly-cloudy-night"
+				return "weather_partly_cloudy_night"
 			case .overcastDay:
-				return "weather_overcast-day"
+				return "weather_overcast_day"
 			case .overcastNight:
-				return "weather_overcast-night"
+				return "weather_overcast_night"
 			case .thunderstormsOvercastRain:
-				return "weather_thunderstorms-overcast-rain"
+				return "weather_thunderstorms_overcast_rain"
 			case .success:
 				return "success"
 			case .fail:
@@ -101,81 +101,81 @@ enum AnimationsEnums: String, CaseIterable {
 			case .overcast:
 				return "weather_overcast"
 			case .overcastRain:
-				return "weather_overcast-rain"
+				return "weather_overcast_rain"
 			case .overcastSnow:
-				return "weather_overcast-snow"
+				return "weather_overcast_snow"
 			case .overcastDrizzle:
-				return "weather_overcast-drizzle"
+				return "weather_overcast_drizzle"
 			case .overcastLightSnow:
-				return "weather_overcast-light-snow"
+				return "weather_overcast_light_snow"
 			case .overcastSleet:
-				return "weather_overcast-sleet"
+				return "weather_overcast_sleet"
 			case .hazeDay:
-				return "weather_haze-day"
+				return "weather_haze_day"
 			case .hazeNight:
-				return "weather_haze-night"
+				return "weather_haze_night"
 			case .extremeDay:
-				return "weather_extreme-day"
+				return "weather_extreme_day"
 			case .extremeNight:
-				return "weather_extreme-night"
+				return "weather_extreme_night"
 			case .extremeRain:
-				return "weather_extreme-rain"
+				return "weather_extreme_rain"
 			case .extremeSnow:
-				return "weather_extreme-snow"
+				return "weather_extreme_snow"
 			case .extremeDaySleet:
-				return "weather_extreme-day-sleet"
+				return "weather_extreme_day_sleet"
 			case .extremeNightSleet:
-				return "weather_extreme-night-sleet"
+				return "weather_extreme_night_sleet"
 			case .extremeDayRain:
-				return "weather_extreme-day-rain"
+				return "weather_extreme_day_rain"
 			case .extremeNightRain:
-				return "weather_extreme-night-rain"
+				return "weather_extreme_night_rain"
 			case .extremeDaySnow:
-				return "weather_extreme-day-snow"
+				return "weather_extreme_day_snow"
 			case .extremeNightSnow:
-				return "weather_extreme-night-snow"
+				return "weather_extreme_night_snow"
 			case .extremeDayDrizzle:
-				return "weather_extreme-day-drizzle"
+				return "weather_extreme_day_drizzle"
 			case .extremeNightDrizzle:
-				return "weather_extreme-night-drizzle"
+				return "weather_extreme_night_drizzle"
 			case .extremeDayLightSnow:
-				return "weather_extreme-day-light-snow"
+				return "weather_extreme_day_light_snow"
 			case .extremeNightLightSnow:
-				return "weather_extreme-night-light-snow"
+				return "weather_extreme_night_light_snow"
 			case .dustDay:
-				return "weather_dust-day"
+				return "weather_dust_day"
 			case .dustNight:
-				return "weather_dust-night"
+				return "weather_dust_night"
 			case .dustWind:
-				return "weather_dust-wind"
+				return "weather_dust_wind"
 			case .thunderstormsLightRain:
-				return "weather_thunderstorms-light-rain"
+				return "weather_thunderstorms_light_rain"
 			case .thunderstormsExtremeRain:
-				return "weather_thunderstorms-extreme-rain"
+				return "weather_thunderstorms_extreme_rain"
 			case .partlyCloudyDayDrizzle:
-				return "weather_partly-cloudy-day-drizzle"
+				return "weather_partly_cloudy_day_drizzle"
 			case .partlyCloudyNightDrizzle:
-				return "weather_partly-cloudy-night-drizzle"
+				return "weather_partly_cloudy_night_drizzle"
 			case .partlyCloudyDaySnow:
-				return "weather_partly-cloudy-day-snow"
+				return "weather_partly_cloudy_day_snow"
 			case .partlyCloudyNightSnow:
-				return "weather_partly-cloudy-night-snow"
+				return "weather_partly_cloudy_night_snow"
 			case .drizzle:
-				return "anim_weather_drizzle"
+				return "deprecated_weather_drizzle"
 			case .rain:
-				return "anim_weather_rain"
+				return "deprecated_weather_rain"
 			case .thunderstormsRain:
-				return "anim_weather_thunderstorms_rain"
+				return "deprecated_weather_thunderstorms_rain"
 			case .snow:
-				return "anim_weather_snow"
+				return "deprecated_weather_snow"
 			case .sleet:
-				return "anim_weather_sleet"
+				return "deprecated_weather_sleet"
 			case .wind:
-				return "anim_weather_wind"
+				return "deprecated_weather_wind"
 			case .fog:
-				return "anim_weather_fog"
+				return "deprecated_weather_fog"
 			case .cloudy:
-				return "anim_weather_cloudy"
+				return "deprecated_weather_cloudy"
 		}
 	}
 }
