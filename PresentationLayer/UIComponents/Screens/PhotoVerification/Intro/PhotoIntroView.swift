5abbf3bb817538b0a6924927e39d23e78239b24f
Pantelis Giazitsis <pgiazitsis@weatherxm.com>
2024-12-04 11:14:12 +0200

    Added view

---
+++ PresentationLayer/UIComponents/Screens/PhotoVerification/Intro/PhotoIntroView.swift
@@ -0,0 +1,18 @@
+//
+//  PhotoIntroView.swift
+//  wxm-ios
+//
+//  Created by Pantelis Giazitsis on 4/12/24.
+//
+
+import SwiftUI
+
+struct PhotoIntroView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    PhotoIntroView()
+}
---
+++ PresentationLayer/UIComponents/Screens/PhotoVerification/Intro/PhotoIntroViewModel.swift
@@ -0,0 +1,13 @@
+//
+//  PhotoIntroViewModel.swift
+//  wxm-ios
+//
+//  Created by Pantelis Giazitsis on 4/12/24.
+//
+
+import Foundation
+
+@MainActor
+class PhotoIntroViewModel: ObservableObject {
+	
+}
