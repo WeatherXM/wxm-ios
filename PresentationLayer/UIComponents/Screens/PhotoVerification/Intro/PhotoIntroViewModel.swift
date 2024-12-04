697d9d9cad816624a3e59fc182de9dc89d7d91e4
Pantelis Giazitsis <pgiazitsis@weatherxm.com>
2024-12-04 14:28:14 +0200

    WIP

--- PresentationLayer/UIComponents/Screens/PhotoVerification/Intro/PhotoIntroViewModel.swift
+++ PresentationLayer/UIComponents/Screens/PhotoVerification/Intro/PhotoIntroViewModel.swift
@@ -9,5 +9,12 @@ import Foundation
 
 @MainActor
 class PhotoIntroViewModel: ObservableObject {
-	
+
+	lazy var instructions: [PhotoIntroView.Instruction] = {
+		[.init(icon: .iconRotate,
+			   text: LocalizableString.PhotoVerification.rotateInstruction.localized),
+		 .init(icon: .iconSurface, text: LocalizableString.PhotoVerification.surfaceInstruction.localized),
+		 .init(icon: .iconNoFaces, text: LocalizableString.PhotoVerification.noFacesInstruction.localized),
+		 .init(icon: .iconMaxPhotos, text: LocalizableString.PhotoVerification.maxPhotosInstruction.localized)]
+	}()
 }
