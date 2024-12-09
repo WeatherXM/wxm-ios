d105b2655346c3facec447fc4503aad66e090bac
Pantelis Giazitsis <pgiazitsis@weatherxm.com>
2024-12-09 14:58:05 +0200

    Added galler view (WIP)

--- PresentationLayer/UIComponents/ViewModelsFactory.swift
+++ PresentationLayer/UIComponents/ViewModelsFactory.swift
@@ -265,4 +265,8 @@ enum ViewModelsFactory {
 	static func getPhotoIntroViewModel() -> PhotoIntroViewModel {
 		return PhotoIntroViewModel()
 	}
+
+	static func getGalleryViewModel() -> GalleryViewModel {
+		return GalleryViewModel()
+	}
 }
