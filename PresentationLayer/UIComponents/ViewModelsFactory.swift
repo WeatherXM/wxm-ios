3c47d7d68378211953cf82c4bbd7ac666a6a3037
Pantelis Giazitsis <pgiazitsis@weatherxm.com>
2024-12-10 13:54:08 +0200

    Save image process

--- PresentationLayer/UIComponents/ViewModelsFactory.swift
+++ PresentationLayer/UIComponents/ViewModelsFactory.swift
@@ -276,6 +276,7 @@ enum ViewModelsFactory {
 	}
 
 	static func getGalleryViewModel() -> GalleryViewModel {
-		return GalleryViewModel()
+		let useCase = SwinjectHelper.shared.getContainerForSwinject().resolve(PhotoGalleryUseCase.self)!
+		return GalleryViewModel(photoGalleryUseCase: useCase)
 	}
 }
