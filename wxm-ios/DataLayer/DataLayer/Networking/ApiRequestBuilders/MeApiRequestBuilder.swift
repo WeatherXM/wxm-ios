1b5441963f613ce36c2da801913666cd08546a20
Pantelis Giazitsis <pgiazitsis@weatherxm.com>
2024-12-17 14:06:57 +0200

    Mock claim repsonse

--- wxm-ios/DataLayer/DataLayer/Networking/ApiRequestBuilders/MeApiRequestBuilder.swift
+++ wxm-ios/DataLayer/DataLayer/Networking/ApiRequestBuilders/MeApiRequestBuilder.swift
@@ -186,6 +186,8 @@ enum MeApiRequestBuilder: URLRequestConvertible {
 extension MeApiRequestBuilder: MockResponseBuilder {
 	var mockFileName: String? {
 		switch self {
+			case .claimDevice:
+				return "claim_device"
 			case .getDevices:
 				return "get_user_devices"
 			case .getUserDeviceById:
