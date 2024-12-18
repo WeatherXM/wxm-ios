24c40511b99804d0f7d8d4f1ecb2e923f806e374
Pantelis Giazitsis <pgiazitsis@weatherxm.com>
2024-12-18 16:56:41 +0200

    Photo state view (WIP)

---
+++ PresentationLayer/UIComponents/Screens/DeviceInfo/PhotoVerificationState/PhotoVerificationStateView.swift
@@ -0,0 +1,72 @@
+//
+//  PhotoVerificationStateView.swift
+//  wxm-ios
+//
+//  Created by Pantelis Giazitsis on 18/12/24.
+//
+
+import SwiftUI
+import NukeUI
+
+struct PhotoVerificationStateView: View {
+	@StateObject var viewModel: PhotoVerificationStateViewModel
+
+	var body: some View {
+		switch viewModel.state {
+			case .content(let photos):
+				photosView(photos: photos)
+			case .uploading(let progress):
+				Text(verbatim: "Uploading")
+			case .empty:
+				Text(verbatim: "Empty")
+		}
+    }
+}
+
+extension PhotoVerificationStateView {
+	enum State {
+		case content(photos: [URL])
+		case uploading(progress: CGFloat)
+		case empty
+	}
+
+	@ViewBuilder
+	func photosView(photos: [URL]) -> some View {
+		let last = photos.last
+		LazyVGrid(columns: [.init(spacing: CGFloat(.smallSpacing)), .init()]) {
+			ForEach(photos, id: \.self) { url in
+				Color.clear
+					.aspectRatio(1.0, contentMode: .fit)
+					.overlay {
+						LazyImage(url: url) { state in
+							if let image = state.image {
+								image
+									.resizable()
+									.aspectRatio(contentMode: .fill)
+									.clipped()
+							} else {
+								ProgressView()
+							}
+						}
+					}
+					.overlay {
+						let isLast = url == last
+						if isLast, viewModel.morePhotosCount > 0 {
+							ZStack {
+								Text("+\(viewModel.morePhotosCount)")
+									.font(.system(size: CGFloat(.XLTitleFontSize), weight: .bold))
+									.foregroundStyle(Color(colorEnum: .textInverse))
+
+								Color.black.opacity(0.6)
+							}
+						}
+					}
+					.cornerRadius(CGFloat(.buttonCornerRadius))
+			}
+		}
+	}
+}
+
+#Preview {
+	PhotoVerificationStateView(viewModel: ViewModelsFactory.getPhotoVerificationStateViewModel(deviceId: ""))
+}
