aba1caa6a50c4ab12c0cef19df4fabd312c7221e
Pantelis Giazitsis <pgiazitsis@weatherxm.com>
2025-01-14 10:05:02 +0200

    Back to camera

--- PresentationLayer/UIComponents/Screens/PhotoVerification/GalleryView/GalleryViewModel.swift
+++ PresentationLayer/UIComponents/Screens/PhotoVerification/GalleryView/GalleryViewModel.swift
@@ -221,7 +221,7 @@ private extension GalleryViewModel {
 		let openPikerCallback = { @MainActor [weak self] in
 			guard let self else { return }
 			let imagePicker = UIImagePickerController()
-			imagePicker.sourceType = .photoLibrary
+			imagePicker.sourceType = .camera
 			imagePicker.delegate = self.imagePickerDelegate
 			UIApplication.shared.topViewController?.present(imagePicker, animated: true)
 		}
