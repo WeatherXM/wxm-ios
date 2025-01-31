07a4a1f96c3eb46630f796ce8e07438cc113dfa9
Pantelis Giazitsis <pgiazitsis@weatherxm.com>
2025-01-31 13:52:00 +0200

    fixup! User info tests

--- WeatherXMTests/DataLayer/Services/UserInfoServiceTests.swift
+++ WeatherXMTests/DataLayer/Services/UserInfoServiceTests.swift
@@ -6,11 +6,71 @@
 //
 
 import Testing
+@testable import DataLayer
+import DomainLayer
+import Toolkit
 
 struct UserInfoServiceTests {
+	let service = UserInfoService()
+	private let cancellableWrapper: CancellableWrapper = .init()
 
-    @Test func <#test function name#>() async throws {
-        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+	@Test func user() async throws {
+		let user = try await service.getUser().toAsync().result.get()
+		#expect(user.email == "user@example.com")
+		#expect(user.name == "TestUser")
     }
 
+	@Test func initialUserFromPubliher() async {
+		await confirmation { confirm in
+			service.getLatestUserInfoPublisher().sink { user in
+				#expect(user == nil)
+				confirm()
+			}.store(in: &cancellableWrapper.cancellableSet)
+		}
+	}
+
+	@Test func userFromPubliher() async {
+		await confirmation { confirm in
+			service.getLatestUserInfoPublisher()
+				.dropFirst()
+				.sink { user in
+				#expect(user?.email == "user@example.com")
+				#expect(user?.name == "TestUser")
+				confirm()
+			}.store(in: &cancellableWrapper.cancellableSet)
+
+			let _ = try? await service.getUser().toAsync()
+		}
+	}
+
+	@Test func userFromPubliherAfterWalletChange() async {
+		await confirmation { confirm in
+			service.getLatestUserInfoPublisher()
+				.dropFirst()
+				.sink { user in
+				#expect(user?.email == "user@example.com")
+				#expect(user?.name == "TestUser")
+				confirm()
+			}.store(in: &cancellableWrapper.cancellableSet)
+
+			let _ = try? await service.saveUserWallet(address: "").toAsync()
+			try? await Task.sleep(for: .seconds(2))
+		}
+	}
+
+	@Test func userFromPubliherAfterLogout() async {
+		await confirmation { @MainActor confirm in
+			service.getLatestUserInfoPublisher()
+				.dropFirst()
+				.sink { user in
+					#expect(user == nil)
+					confirm()
+				}.store(in: &cancellableWrapper.cancellableSet)
+
+			NotificationCenter.default.post(name: .keychainHelperServiceUserIsLoggedChanged,
+											object: false)
+
+			try? await Task.sleep(for: .seconds(2))
+		}
+	}
 }
