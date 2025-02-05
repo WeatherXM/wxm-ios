2bc9fdbfaab95b7e4fc08336ccf71c765f19e468
Pantelis Giazitsis <pgiazitsis@weatherxm.com>
2025-02-05 16:45:29 +0200

    Keychain tests

---
+++ WeatherXMTests/DataLayer/Services/KeychainHelperServiceTests.swift
@@ -0,0 +1,85 @@
+//
+//  KeychainHelperServiceTests.swift
+//  WeatherXMTests
+//
+//  Created by Pantelis Giazitsis on 5/2/25.
+//
+
+import Testing
+@testable import DataLayer
+import DomainLayer
+import Toolkit
+
+@Suite(.serialized)
+struct KeychainHelperServiceTests {
+	let service = KeychainHelperService()
+	let cancellableWrapper: CancellableWrapper
+
+	init() async throws {
+		cancellableWrapper  = .init()
+		service.deleteNetworkTokenResponseFromKeychain()
+		service.deleteEmailAndPassword()
+	}
+
+    @Test func logIn() async throws {
+		validateLoggedIn(isLoggedIn: false)
+
+		try await confirmation { confirm in
+			NotificationCenter.default.publisher(for: .keychainHelperServiceUserIsLoggedChanged).receive(on: DispatchQueue.main).sink { [confirm] notification in
+					guard let isLoggedIn = notification.object as? Bool else {
+						Issue.record("Notification should contain Bool object")
+						return
+					}
+
+					#expect(isLoggedIn == true)
+
+					confirm()
+				}.store(in: &cancellableWrapper.cancellableSet)
+
+			// Log in
+			let response = NetworkTokenResponse(token: "token", refreshToken: "refreshToken")
+			service.saveNetworkTokenResponseToKeychain(item: response)
+			try await Task.sleep(for: .seconds(1))
+		}
+	}
+
+	@Test func logOut() async throws {
+		validateLoggedIn(isLoggedIn: false)
+
+		// Log in
+		let response = NetworkTokenResponse(token: "token", refreshToken: "refreshToken")
+		service.saveNetworkTokenResponseToKeychain(item: response)
+
+		try await confirmation { confirm in
+			NotificationCenter.default.publisher(for: .keychainHelperServiceUserIsLoggedChanged).receive(on: DispatchQueue.main).sink { notification in
+				guard let isLoggedIn = notification.object as? Bool else {
+					Issue.record("Notification should contain Bool object")
+					return
+				}
+
+				#expect(isLoggedIn == false)
+
+				confirm()
+			}.store(in: &cancellableWrapper.cancellableSet)
+
+			// Log out
+			service.deleteNetworkTokenResponseFromKeychain()
+			try await Task.sleep(for: .seconds(1))
+		}
+	}
+
+	@Test func storeEmail() {
+		#expect(service.getUsersAccountInfo() == nil)
+		service.saveEmailAndPasswordToKeychain(email: "email", password: "password")
+		let accountInfo = service.getUsersAccountInfo()
+		#expect(accountInfo?.email == "email")
+		#expect(accountInfo?.password == "password")
+	}
+}
+
+private extension KeychainHelperServiceTests {
+	func validateLoggedIn(isLoggedIn: Bool) {
+		let loggedInState = service.isUserLoggedIn()
+		#expect(loggedInState == isLoggedIn)
+	}
+}
