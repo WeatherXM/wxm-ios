768a33c3da4706fd1e28dc5c2492feaa5871f81a
Pantelis Giazitsis <pgiazitsis@weatherxm.com>
2025-02-06 17:13:41 +0200

    Network repository implementation tests

---
+++ WeatherXMTests/DataLayer/RepositoryImplementations/NetworkRepositoryImplTests.swift
@@ -0,0 +1,72 @@
+//
+//  NetworkRepositoryImplTests.swift
+//  WeatherXMTests
+//
+//  Created by Pantelis Giazitsis on 6/2/25.
+//
+
+import Testing
+@testable import DataLayer
+import DomainLayer
+
+struct NetworkRepositoryImplTests {
+
+	let repositoryImpl: NetworkRepositoryImpl = .init()
+	private let device: NetworkSearchDevice
+	private let address: NetworkSearchAddress
+
+	init() {
+		device = .init(id: "123",
+					   name: "Device",
+					   bundle: .init(name: .d1,
+									 title: "Bundle",
+									 connectivity: .helium,
+									 wsModel: "100",
+									 gwModel: "200",
+									 hwClass: "300"),
+					   cellIndex: "123456",
+					   cellCenter: .init(lat: 0.0, long: 0.0))
+		address = .init(name: "Address",
+						place: "Place",
+						center: .init(lat: 0.0, long: 0.0))
+		repositoryImpl.deleteAllRecent()
+
+	}
+
+    @Test func insertSearchResult() async throws {
+		insertInitialItems()
+    }
+
+	@Test func clearSearchResult() async throws {
+		insertInitialItems()
+		repositoryImpl.deleteAllRecent()
+		let recent = repositoryImpl.getSearchRecent()
+		#expect(recent.isEmpty)
+	}
+
+	@Test func maxLimit() throws {
+		let count = 15
+		for num in 0..<count {
+			let address = NetworkSearchAddress(name: "\(num)",
+											   place: "Place \(num)",
+											   center: .init(lat: 0.0, long: 0.0))
+			repositoryImpl.insertAddressInDB(address)
+		}
+
+		let recent = repositoryImpl.getSearchRecent()
+		#expect(recent.count == 10)
+		#expect((recent.first as? NetworkSearchAddress)?.name == "\(count - 1)")
+	}
+}
+
+private extension NetworkRepositoryImplTests {
+	func insertInitialItems() {
+		repositoryImpl.insertDeviceInDB(device)
+		repositoryImpl.insertAddressInDB(address)
+
+		let recent = repositoryImpl.getSearchRecent()
+		#expect(recent.count == 2)
+		#expect((recent.first as? NetworkSearchAddress)?.name == address.name)
+		#expect((recent.last as? NetworkSearchDevice)?.id == device.id)
+	}
+}
