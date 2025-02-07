6427320a1313456ce7d4dc25806071367cf3cf07
Pantelis Giazitsis <pgiazitsis@weatherxm.com>
2025-02-07 14:00:02 +0200

    Remote config tests

---
+++ WeatherXMTests/DataLayer/RepositoryImplementations/RemoteConfigRepositoryImplTests.swift
@@ -0,0 +1,60 @@
+//
+//  RemoteConfigRepositoryImplTests.swift
+//  WeatherXMTests
+//
+//  Created by Pantelis Giazitsis on 7/2/25.
+//
+
+import Testing
+@testable import DataLayer
+import Toolkit
+
+@Suite(.serialized)
+struct RemoteConfigRepositoryImplTests {
+	private let repositoryImpl: RemoteConfigRepositoryImpl = .init()
+	private let cancellableWrapper: CancellableWrapper = .init()
+
+	init() {
+		UserDefaultsService().clearUserSensitiveData()
+	}
+
+    @Test func survey() async throws {
+		var count = 3
+		try await confirmation(expectedCount: count) { confirm in
+			repositoryImpl.surveyPublisher.sink { survey in
+				print(count)
+				if count == 2 {
+					#expect(survey?.id == "Dummy Text")
+				} else {
+					#expect(survey == nil)
+				}
+				count -= 1
+
+				confirm()
+			}.store(in: &cancellableWrapper.cancellableSet)
+			try await Task.sleep(for: .seconds(2))
+			repositoryImpl.updateLastSurveyId("Dummy Text")
+			try await Task.sleep(for: .seconds(1))
+		}
+    }
+
+	@Test func infoBanner() async throws {
+		var count = 3
+		try await confirmation(expectedCount: count) { confirm in
+			repositoryImpl.infoBannerPublisher.sink { infoBanner in
+				print(count)
+				if count == 2 {
+					#expect(infoBanner?.id == "Dummy Text")
+				} else {
+					#expect(infoBanner == nil)
+				}
+				count -= 1
+
+				confirm()
+			}.store(in: &cancellableWrapper.cancellableSet)
+			try await Task.sleep(for: .seconds(2))
+			repositoryImpl.updateLastDismissedInfoBannerId("Dummy Text")
+			try await Task.sleep(for: .seconds(1))
+		}
+	}
+}
