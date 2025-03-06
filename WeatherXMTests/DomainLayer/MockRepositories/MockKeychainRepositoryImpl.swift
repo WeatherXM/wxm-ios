696299c73446bb8a6895e11ac241c9b6308c9d5f
Pantelis Giazitsis <pgiazitsis@weatherxm.com>
2025-03-06 16:22:51 +0200

    Mock repo implementations

---
+++ WeatherXMTests/DomainLayer/MockRepositories/MockKeychainRepositoryImpl.swift
@@ -0,0 +1,48 @@
+//
+//  MockKeychainRepositoryImpl.swift
+//  WeatherXMTests
+//
+//  Created by Pantelis Giazitsis on 6/3/25.
+//
+
+import Foundation
+@testable import DomainLayer
+
+class MockKeychainRepositoryImpl {
+	private var tokenResponse: NetworkTokenResponse?
+	private var email: String?
+	private var password: String?
+}
+extension MockKeychainRepositoryImpl: KeychainRepository {
+	var userLoggedInStateNotificationPublisher: NotificationCenter.Publisher {
+		NotificationCenter.default.publisher(for: Notification.Name("MockKeychainRepositoryImpl.userIsLoggedInChanged"))
+	}
+
+	func saveNetworkTokenResponseToKeychain(item: NetworkTokenResponse) {
+		tokenResponse = item
+	}
+	
+	func isUserLoggedIn() -> Bool {
+		tokenResponse != nil
+	}
+	
+	func deleteNetworkTokenResponseFromKeychain() {
+		tokenResponse = nil
+	}
+	
+	func saveEmailAndPasswordToKeychain(email: String, password: String) {
+		self.email = email
+		self.password = password
+	}
+	
+	func deleteEmailAndPasswordFromKeychain() {
+		self.email = nil
+		self.password = nil
+	}
+	
+	func getUsersEmail() -> String {
+		email ?? ""
+	}
+	
+
+}
