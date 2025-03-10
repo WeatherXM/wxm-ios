99bf875195035b58dfea78a98d93d5e7c35d5191
Pantelis Giazitsis <pgiazitsis@weatherxm.com>
2025-03-10 16:45:35 +0200

    Device location use case tests

---
+++ WeatherXMTests/DomainLayer/UseCases/DeviceLocationUseCaseTests.swift
@@ -0,0 +1,16 @@
+//
+//  DeviceLocationUseCaseTests.swift
+//  WeatherXMTests
+//
+//  Created by Pantelis Giazitsis on 10/3/25.
+//
+
+import Testing
+
+struct DeviceLocationUseCaseTests {
+
+    @Test func <#test function name#>() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
