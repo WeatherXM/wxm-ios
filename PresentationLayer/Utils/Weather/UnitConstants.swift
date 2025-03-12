ba320c7988bd5372f8edf659285025719f0c3b60
Pantelis Giazitsis <pgiazitsis@weatherxm.com>
2025-03-12 17:39:36 +0200

    Transform unit strings to prevent line breaks

--- PresentationLayer/Utils/Weather/UnitConstants.swift
+++ PresentationLayer/Utils/Weather/UnitConstants.swift
@@ -6,25 +6,26 @@
 //
 
 public enum UnitConstants {
-	public static let CELCIUS = LocalizableString.Units.celsiusSymbol.localized
-    public static let FAHRENHEIT = LocalizableString.Units.fahrenheitSymbol.localized
-    public static let MILLIMETERS = LocalizableString.Units.millimetersSymbol.localized
-    public static let INCHES = LocalizableString.Units.inchesSymbol.localized
-    public static let KILOMETERS_PER_HOUR = LocalizableString.Units.kilometersPerHourSymbol.localized
-    public static let MILES_PER_HOUR = LocalizableString.Units.milesPerHourSymbol.localized
-    public static let METERS_PER_SECOND = LocalizableString.Units.metersPerSecondSymbol.localized
-    public static let KNOTS = LocalizableString.Units.knotsSymbol.localized
-    public static let BEAUFORT = LocalizableString.Units.beaufortSymbol.localized
-    public static let CARDINAL = LocalizableString.Units.cardinalSymbol.localized
-    public static let DEGREES = LocalizableString.Units.degreesSymbol.localized
-    public static let HECTOPASCAL = LocalizableString.Units.hectopascalSymbol.localized
-    public static let INCH_OF_MERCURY = LocalizableString.Units.inchOfMercurySymbol.localized
-    public static let UV = LocalizableString.Units.uvSymbol.localized
-    public static let PERCENT = "%"
-    public static let MILLIMETERS_PER_HOUR = LocalizableString.Units.millimetersPerHourSymbol.localized
-    public static let INCHES_PER_HOUR = LocalizableString.Units.inchesPerHourSymbol.localized
-    public static let WATTS_PER_SQR = LocalizableString.Units.wattsPerSquareSymbol.localized
-    public static let DBM = LocalizableString.Units.dBmSymbol.localized
+	public static let CELCIUS = LocalizableString.Units.celsiusSymbol.localized.fixedLinebreaks
+	public static let FAHRENHEIT = LocalizableString.Units.fahrenheitSymbol.localized.fixedLinebreaks
+	public static let MILLIMETERS = LocalizableString.Units.millimetersSymbol.localized.fixedLinebreaks
+	public static let INCHES = LocalizableString.Units.inchesSymbol.localized.fixedLinebreaks
+	public static let KILOMETERS_PER_HOUR = LocalizableString.Units.kilometersPerHourSymbol.localized.fixedLinebreaks
+	public static let MILES_PER_HOUR = LocalizableString.Units.milesPerHourSymbol.localized.fixedLinebreaks
+	public static let METERS_PER_SECOND = LocalizableString.Units.metersPerSecondSymbol.localized.fixedLinebreaks
+	public static let KNOTS = LocalizableString.Units.knotsSymbol.localized.fixedLinebreaks
+	public static let BEAUFORT = LocalizableString.Units.beaufortSymbol.localized.fixedLinebreaks
+	public static let CARDINAL = LocalizableString.Units.cardinalSymbol.localized.fixedLinebreaks
+	public static let DEGREES = LocalizableString.Units.degreesSymbol.localized.fixedLinebreaks
+	public static let HECTOPASCAL = LocalizableString.Units.hectopascalSymbol.localized.fixedLinebreaks
+	public static let INCH_OF_MERCURY = LocalizableString.Units.inchOfMercurySymbol.localized.fixedLinebreaks
+	public static let UV = LocalizableString.Units.uvSymbol.localized.fixedLinebreaks
+    public static let PERCENT = "%".fixedLinebreaks
+	public static let MILLIMETERS_PER_HOUR = LocalizableString.Units.millimetersPerHourSymbol.localized.fixedLinebreaks
+	public static let INCHES_PER_HOUR = LocalizableString.Units.inchesPerHourSymbol.localized.fixedLinebreaks
+	public static let WATTS_PER_SQR = LocalizableString.Units.wattsPerSquareSymbol.localized.fixedLinebreaks
+	public static let DBM = LocalizableString.Units.dBmSymbol.localized.fixedLinebreaks
+}
 
 private extension String {
 	var fixedLinebreaks: String {
