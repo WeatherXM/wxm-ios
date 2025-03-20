1033a5420e3b8e2fdc6eaf92396ea51765fee945
Pantelis Giazitsis <pgiazitsis@weatherxm.com>
2025-03-20 13:11:46 +0200

    Removed unnecessary parameter

--- PresentationLayer/UIComponents/Screens/RewardBoosts/RewardBoostsView.swift
+++ PresentationLayer/UIComponents/Screens/RewardBoosts/RewardBoostsView.swift
@@ -38,8 +38,7 @@ struct RewardBoostsView: View {
 			.iPadMaxWidth()
 			.onAppear {
 				navigationObject.navigationBarColor = Color(colorEnum: .background)
-				WXMAnalytics.shared.trackScreen(.boostDetail,
-												parameters: [.itemId: .custom(viewModel.response?.code?.rawValue ?? "")])
+				WXMAnalytics.shared.trackScreen(.boostDetail)
 			}
 		}
     }
