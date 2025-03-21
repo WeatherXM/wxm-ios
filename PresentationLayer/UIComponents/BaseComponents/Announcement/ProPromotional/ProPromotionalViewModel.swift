48dd800627cb65488287c54271b6f6022bd4a89f
Pantelis Giazitsis <pgiazitsis@weatherxm.com>
2025-03-21 13:14:52 +0200

    Bottom sheet (WIP)

---
+++ PresentationLayer/UIComponents/BaseComponents/Announcement/ProPromotional/ProPromotionalViewModel.swift
@@ -0,0 +1,12 @@
+//
+//  ProPromotionalViewModel.swift
+//  wxm-ios
+//
+//  Created by Pantelis Giazitsis on 21/3/25.
+//
+
+import Foundation
+
+class ProPromotionalViewModel: ObservableObject {
+	
+}
