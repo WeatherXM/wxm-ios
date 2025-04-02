531fa569b48e880d8f27c697c67b7e1b9c81a406
Pantelis Giazitsis <pgiazitsis@weatherxm.com>
2025-04-02 17:10:43 +0300

    Explorer stations list view model tests

---
+++ WeatherXMTests/PresentationLayer/ViewModels/ExplorerStationsListViewModelTests.swift
@@ -0,0 +1,30 @@
+//
+//  ExplorerStationsListViewModelTests.swift
+//  WeatherXMTests
+//
+//  Created by Pantelis Giazitsis on 2/4/25.
+//
+
+import Testing
+@testable import WeatherXM
+import DomainLayer
+
+@MainActor
+struct ExplorerStationsListViewModelTests {
+
+	let viewModel: ExplorerStationsListViewModel
+	let useCase: MockExplorerUseCase
+
+	init() {
+		useCase = MockExplorerUseCase()
+		viewModel = ExplorerStationsListViewModel(useCase: useCase,
+												  cellIndex: "",
+												  cellCenter: nil)
+	}
+
+    @Test func cellInfo() async throws {
+		#expect(!viewModel.showInfo)
+		viewModel.handleCellCapacityInfoTap()
+		#expect(viewModel.showInfo)
+    }
+}
