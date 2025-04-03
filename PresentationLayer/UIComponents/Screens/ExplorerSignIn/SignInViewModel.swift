c8e8c2a2d7df34aaee5d2dff1e094370cb7b0eec
Pantelis Giazitsis <pgiazitsis@weatherxm.com>
2025-04-03 10:57:45 +0300

    Sign in view model tests

--- PresentationLayer/UIComponents/Screens/ExplorerSignIn/SignInViewModel.swift
+++ PresentationLayer/UIComponents/Screens/ExplorerSignIn/SignInViewModel.swift
@@ -13,7 +13,6 @@ import Toolkit
 final class SignInViewModel: ObservableObject {
     @Published var email: String = ""
     @Published var password: String = ""
-    @Published var tokenResponse = NetworkTokenResponse()
     @Published var isSignInButtonAvailable: Bool = false
     private var cancellableSet: Set<AnyCancellable> = []
 	private final let authUseCase: AuthUseCaseApi
---
+++ WeatherXMTests/PresentationLayer/ViewModels/SignInViewModelTests.swift
@@ -0,0 +1,62 @@
+//
+//  SignInViewModelTests.swift
+//  WeatherXMTests
+//
+//  Created by Pantelis Giazitsis on 3/4/25.
+//
+
+import Testing
+@testable import WeatherXM
+
+@MainActor
+struct SignInViewModelTests {
+	let viewModel: SignInViewModel
+	let authUseCase: MockAuthUseCase
+
+	init() {
+		authUseCase = .init()
+		viewModel = .init(authUseCase: authUseCase)
+	}
+
+	@Test func loginWithValidCredentials() async throws {
+		viewModel.email = "test@example.com"
+		viewModel.password = "correctPassword"
+		viewModel.login { error in
+			#expect(error == nil)
+		}
+		try await Task.sleep(for: .seconds(2))
+		#expect(viewModel.isSignInButtonAvailable)
+	}
+
+	@Test func loginWithInvalidCredentials() async throws {
+		viewModel.email = "test@example.com"
+		viewModel.password = MockAuthUseCase.wrongPassword
+		viewModel.login { error in
+			#expect(error != nil)
+		}
+		try await Task.sleep(for: .seconds(2))
+		#expect(viewModel.isSignInButtonAvailable)
+	}
+
+	@Test func checkSignInButtonAvailability() {
+		viewModel.email = ""
+		viewModel.password = ""
+		viewModel.checkSignInButtonAvailability()
+		#expect(!viewModel.isSignInButtonAvailable)
+
+		viewModel.email = "test@example.com"
+		viewModel.password = ""
+		viewModel.checkSignInButtonAvailability()
+		#expect(!viewModel.isSignInButtonAvailable)
+
+		viewModel.email = ""
+		viewModel.password = "correctPassword"
+		viewModel.checkSignInButtonAvailability()
+		#expect(!viewModel.isSignInButtonAvailable)
+
+		viewModel.email = "test@example.com"
+		viewModel.password = "correctPassword"
+		viewModel.checkSignInButtonAvailability()
+		#expect(viewModel.isSignInButtonAvailable)
+	}
+}
