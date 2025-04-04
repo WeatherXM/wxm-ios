5fd6438f5876e54ca6b1f943d8b986436a4b655a
Pantelis Giazitsis <pgiazitsis@weatherxm.com>
2025-04-04 11:19:16 +0300

    Reward annotations tests

---
+++ WeatherXMTests/PresentationLayer/ViewModels/RewardAnnotationsViewModelTests.swift
@@ -0,0 +1,50 @@
+//
+//  RewardAnnotationsViewModelTests.swift
+//  WeatherXMTests
+//
+//  Created by Pantelis Giazitsis on 4/4/25.
+//
+
+import Testing
+import DomainLayer
+@testable import WeatherXM
+
+@MainActor
+struct RewardAnnotationsViewModelTests {
+	let viewModel: RewardAnnotationsViewModel
+	let device: DeviceDetails
+	let annotations: [RewardAnnotation]
+	let followState: UserDeviceFollowState?
+	let refDate: Date
+
+	init() {
+		device = DeviceDetails.mockDevice
+		let docUrl = "https://example.com"
+		annotations = [
+			RewardAnnotation(severity: .info, group: .noWallet, title: "Info", message: "Info message", docUrl: docUrl),
+			RewardAnnotation(severity: .warning, group: .locationNotVerified, title: "Warning", message: "Warning message", docUrl: docUrl),
+			RewardAnnotation(severity: .error, group: .noLocationData, title: "Error", message: "Error message", docUrl: docUrl)
+		]
+		followState = nil
+		refDate = Date()
+		viewModel = .init(device: device,
+						  annotations: annotations,
+						  followState: followState,
+						  refDate: refDate)
+	}
+
+	@Test func annotationActionButtonTile() {
+		annotations.forEach { annotation in
+			let title = viewModel.annotationActionButtonTile(for: annotation)
+			#expect(title != nil)
+			switch annotation.group {
+				case .noWallet:
+					#expect(title == LocalizableString.RewardDetails.readMore.localized)
+				case .locationNotVerified:
+					#expect(title == LocalizableString.RewardDetails.readMore.localized)
+				default:
+					#expect(title == LocalizableString.RewardDetails.readMore.localized)
+			}
+		}
+	}
+}
