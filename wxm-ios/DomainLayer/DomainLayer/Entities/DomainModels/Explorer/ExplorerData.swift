3c833a1bb614aa93de06070a47614959be7026a4
Pantelis Giazitsis <pgiazitsis@weatherxm.com>
2025-05-02 14:27:46 +0300

    Text annotations in explorer data

--- wxm-ios/DomainLayer/DomainLayer/Entities/DomainModels/Explorer/ExplorerData.swift
+++ wxm-ios/DomainLayer/DomainLayer/Entities/DomainModels/Explorer/ExplorerData.swift
@@ -5,23 +5,25 @@
 //  Created by Lampros Zouloumis on 17/8/22.
 //
 
-import struct MapboxMaps.GeoJSONSource
-import struct MapboxMaps.PolygonAnnotation
+import MapboxMaps
 
 public struct ExplorerData {
     public let totalDevices: Int
     public let geoJsonSource: GeoJSONSource
     public let polygonPoints: [PolygonAnnotation]
+	public let textPoints: [PointAnnotation]
 
     public init() {
         self.totalDevices = 0
         self.geoJsonSource = GeoJSONSource(id: "heatmap")
         self.polygonPoints = []
+		self.textPoints = []
     }
 
-    public init(totalDevices: Int, geoJsonSource: GeoJSONSource, polygonPoints: [PolygonAnnotation]) {
+    public init(totalDevices: Int, geoJsonSource: GeoJSONSource, polygonPoints: [PolygonAnnotation], textPoints: [PointAnnotation]) {
         self.totalDevices = totalDevices
         self.geoJsonSource = geoJsonSource
         self.polygonPoints = polygonPoints
+		self.textPoints = textPoints
     }
 }
