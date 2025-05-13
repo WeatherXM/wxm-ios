4d228efc48959330b7193848153f149ec1483810
Pantelis Giazitsis <pgiazitsis@weatherxm.com>
2025-05-13 18:02:26 +0300

    Navigate to token metrics (WIP)

---
+++ PresentationLayer/UIComponents/Screens/NetworkStatistics/TokenMetricsView.swift
@@ -0,0 +1,22 @@
+//
+//  TokenMetricsView.swift
+//  wxm-ios
+//
+//  Created by Pantelis Giazitsis on 13/5/25.
+//
+
+import SwiftUI
+
+struct TokenMetricsView: View {
+    var body: some View {
+		VStack {
+			Spacer()
+			Text(verbatim: "Token Metrics")
+			Spacer()
+		}
+    }
+}
+
+#Preview {
+    TokenMetricsView()
+}
