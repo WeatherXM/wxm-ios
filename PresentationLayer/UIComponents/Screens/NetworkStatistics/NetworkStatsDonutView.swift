76d23ef2231cd9a5fe968c01e44054bb3c09ce02
Pantelis Giazitsis <pgiazitsis@weatherxm.com>
2025-05-15 11:46:16 +0300

    Added donut view

---
+++ PresentationLayer/UIComponents/Screens/NetworkStatistics/NetworkStatsDonutView.swift
@@ -0,0 +1,82 @@
+//
+//  NetworkStatsDonutView.swift
+//  wxm-ios
+//
+//  Created by Pantelis Giazitsis on 15/5/25.
+//
+
+import SwiftUI
+
+struct NetworkStatsDonutView: View {
+	let claimed: Double
+	let reserved: Double
+
+    var body: some View {
+		HStack(spacing: 0.0) {
+			Text(LocalizableString.NetStats.claimed.localized)
+
+			ProgressView(value: claimed, total: reserved)
+				.progressViewStyle(DonutProgressStyle(lineWidth: 26.0,
+													  color: Color(colorEnum: .chartPrimary),
+													  progressColor: Color(colorEnum: .chartSecondary)))
+				.frame(width: 140.0, height: 80.0)
+				.background(.red)
+		}
+    }
+}
+
+struct DonutProgressStyle: ProgressViewStyle {
+	let lineWidth: CGFloat
+	let color: Color
+	let progressColor: Color
+	private let angleThreshold: CGFloat = 180.0
+
+	func makeBody(configuration: Configuration) -> some View {
+		let angleProgress = getAngleProgress(configuration: configuration)
+		ZStack {
+			Arc(startAngle: .degrees(0.0 + angleProgress + angleThreshold),
+				endAngle: .degrees(180.0 + angleThreshold),
+				clockwise: false,
+				lineWidth: lineWidth)
+			.stroke(color, lineWidth: 26)
+
+			Arc(startAngle: .degrees(angleThreshold),
+				endAngle: .degrees(angleThreshold + angleProgress),
+				clockwise: false,
+				lineWidth: lineWidth)
+			.stroke(progressColor, lineWidth: lineWidth)
+		}
+	}
+
+	private func getAngleProgress(configuration: Configuration) -> CGFloat {
+		let completed = configuration.fractionCompleted ?? 0.0
+		let progress = 180.0 * completed
+		return progress
+	}
+}
+
+struct Arc: Shape {
+	let startAngle: Angle
+	let endAngle: Angle
+	let clockwise: Bool
+	let lineWidth: CGFloat
+
+	func path(in rect: CGRect) -> Path {
+		var path = Path()
+
+		let adjustedRadius = (rect.width / 2) - (lineWidth / 2)
+
+		path.addArc(center: CGPoint(x: rect.midX, y: rect.maxY),
+					radius: adjustedRadius,
+					startAngle: startAngle,
+					endAngle: endAngle,
+					clockwise: clockwise)
+
+		return path
+	}
+}
+
+#Preview {
+	NetworkStatsDonutView(claimed: 296, reserved: 1000)
+		.padding()
+}
