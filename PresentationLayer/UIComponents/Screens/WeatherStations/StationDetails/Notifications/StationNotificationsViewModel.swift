877e87fccf13d7e062ec1270c6369df2fd4b24bc
Pantelis Giazitsis <pgiazitsis@weatherxm.com>
2025-07-08 14:27:42 +0300

    WIP

--- PresentationLayer/UIComponents/Screens/WeatherStations/StationDetails/Notifications/StationNotificationsViewModel.swift
+++ PresentationLayer/UIComponents/Screens/WeatherStations/StationDetails/Notifications/StationNotificationsViewModel.swift
@@ -6,7 +6,14 @@
 //
 
 import Foundation
+import DomainLayer
 
 class StationNotificationsViewModel: ObservableObject {
-	
+	let device: DeviceDetails
+	let followState: UserDeviceFollowState
+
+	init(device: DeviceDetails, followState: UserDeviceFollowState) {
+		self.device = device
+		self.followState = followState
+	}
 }
