11415869d9655b56ae46c3449b8da93fdcc349fa
Pantelis Giazitsis <pgiazitsis@weatherxm.com>
2025-07-08 14:51:33 +0300

    UI

--- PresentationLayer/UIComponents/Screens/WeatherStations/StationDetails/Notifications/StationsNotificationsView.swift
+++ PresentationLayer/UIComponents/Screens/WeatherStations/StationDetails/Notifications/StationsNotificationsView.swift
@@ -15,7 +15,26 @@ struct StationsNotificationsView: View {
 		ScrollView {
 			VStack(spacing: CGFloat(.defaultSpacing)) {
 				titleView
-				Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+
+				optionView(title: LocalizableString.StationDetails.showNotifications.localized,
+						   description: nil,
+						   switchOn: Binding(get: {
+					viewModel.valueFromMasterSwitch
+				}, set: { value in
+					viewModel.setmasterSwitchValue(value)
+				}))
+
+				WXMDivider()
+
+				ForEach(StationNotificationsTypes.allCases, id: \.self) { notificationType in
+					optionView(title: notificationType.title,
+							   description: notificationType.description,
+							   switchOn: .init(get: {
+						viewModel.valueFor(notificationType: notificationType)
+					}, set: { value in
+						viewModel.setValue(value, for: notificationType)
+					}))
+				}
 			}
 			.padding(CGFloat(.defaultSidePadding))
 		}
@@ -43,8 +62,34 @@ private extension StationsNotificationsView {
 			Text(viewModel.device.friendlyName ?? "")
 				.foregroundStyle(Color(colorEnum: .text))
 				.font(.system(size: CGFloat(.smallTitleFontSize)))
-			
+
 			Spacer()
+		}
+	}
+
+	@ViewBuilder
+	func optionView(title: String,
+					description: String?,
+					switchOn: Binding<Bool>) -> some View {
+		HStack {
+			VStack(alignment: .leading, spacing: CGFloat(.minimumSpacing)) {
+				Text(title)
+					.foregroundStyle(Color(colorEnum: .text))
+					.font(.system(size: CGFloat(.smallTitleFontSize)))
+
+				if let description {
+					Text(description)
+						.foregroundStyle(Color(colorEnum: .text))
+						.font(.system(size: CGFloat(.normalFontSize)))
+						.fixedSize(horizontal: false, vertical: true)
+				}
+			}
+
+			Spacer()
+
+			Toggle("", isOn: switchOn)
+				.labelsHidden()
+				.toggleStyle(WXMToggleStyle.Default)
 		}
 	}
 }
