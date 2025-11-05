a6eb0a56819454b9acaa32de5c12419d06984b81
Pantelis Giazitsis <pgiazitsis@weatherxm.com>
2025-11-05 12:27:00 +0200

    Added Mosaic card view

---
+++ PresentationLayer/UIComponents/Screens/WeatherStations/StationDetails/Forecast/MosaicCardView.swift
@@ -0,0 +1,67 @@
+//
+//  MosaicCardView.swift
+//  wxm-ios
+//
+//  Created by Pantelis Giazitsis on 4/11/25.
+//
+
+import SwiftUI
+
+struct MosaicCardView: View {
+    var body: some View {
+		VStack(spacing: CGFloat(.largeSpacing)) {
+			VStack(spacing: CGFloat(.smallSpacing)) {
+				Text(LocalizableString.Forecast.mosaic.localized)
+					.font(.system(size: CGFloat(.largeTitleFontSize), weight: .bold))
+					.foregroundStyle(Color(colorEnum: .wxmPrimary))
+
+				Text(LocalizableString.Forecast.smarterSharper.localized)
+					.font(.system(size: CGFloat(.mediumFontSize), weight: .bold))
+					.foregroundStyle(Color(colorEnum: .text))
+
+			}
+
+			Text(LocalizableString.Forecast.mosaicCardDescription.localized)
+				.font(.system(size: CGFloat(.normalFontSize)))
+				.foregroundStyle(Color(colorEnum: .chartsTertiary))
+				.multilineTextAlignment(.center)
+
+			VStack(spacing: CGFloat(.smallSpacing)) {
+				Button {
+					
+				} label: {
+					HStack {
+						Spacer()
+
+						Text(LocalizableString.Forecast.seeThePlans.localized)
+							.font(.system(size: CGFloat(.largeFontSize)))
+							.foregroundStyle(Color(colorEnum: .bg))
+							.multilineTextAlignment(.center)
+
+						Spacer()
+					}
+					.padding(.vertical, CGFloat(.mediumSidePadding))
+				}
+				.background {
+					Capsule()
+						.fill(Color(.wxmPrimary))
+				}
+
+
+				Button {
+
+				} label: {
+					Text(LocalizableString.Forecast.freeSubscriptionText.localized)
+						.font(.system(size: CGFloat(.caption)))
+						.foregroundStyle(Color(colorEnum: .chartsTertiary))
+						.multilineTextAlignment(.center)
+				}
+			}
+		}
+		.WXMCardStyle()
+    }
+}
+
+#Preview {
+    MosaicCardView()
+}
