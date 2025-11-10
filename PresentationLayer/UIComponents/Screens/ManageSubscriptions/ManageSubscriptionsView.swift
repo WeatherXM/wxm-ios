32e52dc1eb0ee270ebd14544d8d5a641738f649a
Pantelis Giazitsis <pgiazitsis@weatherxm.com>
2025-11-10 12:04:30 +0200

    Added files

---
+++ PresentationLayer/UIComponents/Screens/ManageSubscriptions/ManageSubscriptionsView.swift
@@ -0,0 +1,18 @@
+//
+//  ManageSubscriptionsView.swift
+//  wxm-ios
+//
+//  Created by Pantelis Giazitsis on 10/11/25.
+//
+
+import SwiftUI
+
+struct ManageSubscriptionsView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ManageSubscriptionsView()
+}
