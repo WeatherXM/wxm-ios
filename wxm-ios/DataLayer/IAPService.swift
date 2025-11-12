2c2f012e885e26ecbddb7ebdb87361d78fad7aab
Pantelis Giazitsis <pgiazitsis@weatherxm.com>
2025-11-12 15:19:52 +0200

    Fail state

--- wxm-ios/DataLayer/IAPService.swift
+++ wxm-ios/DataLayer/IAPService.swift
@@ -90,5 +90,18 @@ public extension IAPService {
 		case purchaseCancelled
 		case purchaseIsPending
 		case purchaseFailed
+
+		var localiizedDescription: String {
+			switch self {
+				case .noProductWithId(let string):
+					"No product with id: \(string)"
+				case .purchaseCancelled:
+					"Purchase cancelled"
+				case .purchaseIsPending:
+					"Purchase is pending"
+				case .purchaseFailed:
+					"Purchase failed"
+			}
+		}
 	}
 }
